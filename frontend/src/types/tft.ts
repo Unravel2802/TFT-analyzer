8f5b864a7c2089886aa5c9b6ea06557fcf1cbdc3
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2026-05-21 20:57:41 -0400

    add types for account and playerstats

---
+++ frontend/src/types/tft.ts
@@ -0,0 +1,17 @@
+// GET/players/{game_name}/{tag_line}
+export interface Account {
+    puuid: string;
+    gameName: string;
+    tagLine: string
+}
+
+export type UnitEntry = [string, number];
+export type TraitEntry = [string, number];
+
+export interface PlayerStats {
+    avg_placement: number;
+    top4_rate: string;
+    win_rate: string;
+    top_units: UnitEntry[];
+    top_traits: TraitEntry[];
+}
