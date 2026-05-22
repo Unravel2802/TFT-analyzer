7ca895d90aa783e1993204bc2bce9d046c10ca84
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2026-05-22 10:07:02 -0400

    add TopList component

---
+++ frontend/src/components/TopList.tsx
@@ -0,0 +1,22 @@
+import type { UnitEntry, TraitEntry } from '../types/tft'
+
+interface Props {
+    title: string
+    entries: UnitEntry[] | TraitEntry[]
+}
+
+export default function TopList({ title, entries }: Props) {
+    return (
+        <div className="top-list">
+            <h3>{title}</h3>
+            <ul>
+                {entries.map(([name, count]) => (
+                    <li key={name}>
+                        <span>{name}</span>
+                        <span>{count}x</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+} 
